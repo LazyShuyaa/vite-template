2c26e452d61944ff22eed185edc254fde536f77a
LazyShuyaa <168578401+LazyShuyaa@users.noreply.github.com>
2024-12-21 10:39:12 +0530

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -12,7 +12,8 @@ import { link as linkStyles } from "@nextui-org/theme";
 import clsx from "clsx";
 
 import { ThemeSwitch } from "@/components/theme-switch";
-import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import { useTheme } from "@/hooks/useTheme";
 
 export const Navbar = () => {
   const customNavMenuItems: { label: string; href: string }[] = [
@@ -23,19 +24,8 @@ export const Navbar = () => {
     { label: "Project", href: "/project" },
   ];
 
-  const [activeLink, setActiveLink] = useState("");
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
-
-  useEffect(() => {
-    const theme = localStorage.getItem("theme");
-    if (theme === "dark") {
-      setIsDarkTheme(true);
-    }
-  }, []);
-
-  const handleLinkClick = (href: string) => {
-    setActiveLink(href);
-  };
+  const router = useRouter();
+  const { isDark } = useTheme();
 
   return (
     <NextUINavbar maxWidth="xl" position="sticky">
@@ -46,7 +36,7 @@ export const Navbar = () => {
             color="foreground"
             href="/"
           >
-            <p className="font-bold text-inherit">Rugi Empower</p>
+            <p className="font-bold text-inherit">Rugi Empire</p>
           </Link>
         </NavbarBrand>
         <div className="hidden md:flex gap-4 justify-center flex-grow">
@@ -55,11 +45,14 @@ export const Navbar = () => {
               <Link
                 className={clsx(
                   linkStyles({ color: "foreground" }),
-                  activeLink === item.href ? (isDarkTheme ? "bg-white text-black" : "bg-black text-white") : "text-white",
+                  router.pathname === item.href
+                    ? isDark
+                      ? "bg-white text-black"
+                      : "bg-black text-white"
+                    : "text-white",
                   "rounded-full px-2 py-1 transition-colors duration-300"
                 )}
                 href={item.href}
-                onClick={() => handleLinkClick(item.href)}
               >
                 {item.label}
               </Link>
@@ -82,12 +75,15 @@ export const Navbar = () => {
             <NavbarMenuItem key={item.href}>
               <Link
                 className={clsx(
-                  activeLink === item.href ? (isDarkTheme ? "bg-white text-black" : "bg-black text-white") : "text-white",
+                  router.pathname === item.href
+                    ? isDark
+                      ? "bg-white text-black"
+                      : "bg-black text-white"
+                    : "text-white",
                   "rounded-full px-2 py-1 transition-colors duration-300"
                 )}
                 href={item.href}
                 size="lg"
-                onClick={() => handleLinkClick(item.href)}
               >
                 {item.label}
               </Link>
