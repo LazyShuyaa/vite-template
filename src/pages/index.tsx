7fab98cdd0b1c1920685ee9a030055d23d8057d6
LazyShuyaa <168578401+LazyShuyaa@users.noreply.github.com>
2024-12-21 09:54:03 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,14 @@
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
-import { button as buttonStyles } from "@nextui-org/theme";
-
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
 import DefaultLayout from "@/layouts/default";
 
 export default function IndexPage() {
   return (
     <DefaultLayout>
-      
+      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+        <div className="inline-block max-w-lg text-center justify-center">
+          <h1 className="text-3xl font-bold">Under Construction</h1>
+          <p className="mt-4 text-lg">We are working hard to bring you something amazing. Stay tuned!</p>
+        </div>
+      </section>
     </DefaultLayout>
   );
 }
